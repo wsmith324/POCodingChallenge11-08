ed387a4c9059b90bd7f9ff81781c981ec9f70f2f
wsmith324 <william815@revature.net>
2022-11-08 15:47:52 -0500

    submission

--- src/main/java/com/revature/harshad/Main.java
+++ src/main/java/com/revature/harshad/Main.java
@@ -2,6 +2,31 @@ package com.revature.harshad;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Value 75 returns " + isHarshad(75));
+        System.out.println("Value 171 returns " + isHarshad(171));
+        System.out.println("Value 481 returns " + isHarshad(481));
+        System.out.println("Value 89 returns " + isHarshad(89));
+        System.out.println("Value 516 returns " + isHarshad(516));
+        System.out.println("Value 200 returns " + isHarshad(200));
     }
+
+    public static boolean isHarshad(int num) {
+        if (num%sumDigit(num)==0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static int sumDigit(int num) {
+        int sum = 0;
+        if (num>=10) {
+            int lastDigit = num%10;
+            sum += lastDigit + sumDigit((num-lastDigit)/10);
+        } else {
+            sum += num;
+        }
+        return sum;
+    }
+
 }
